81191f36925a9da670764e6db38a4d94dc6be104
henriklindstad <henrik.haugen.lindstad@gmail.com>
2023-08-31 11:02:20 +0200

    Initial commit

---
+++ Temperatur/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturIfElseWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gyldig = false;
+
+            while (!gyldig)
+            {
+                Console.Write("Skriv inn en temperatur: ");
+                string inn = Console.ReadLine();
+
+                if (int.TryParse(inn, out int temp))
+                {
+                    switch (temp)
+                    {
+                        case int n when n > 0:
+                            Console.WriteLine("Det er plussgrader");
+                            break;
+                        case int n when n < 0:
+                            Console.WriteLine("Det er minusgrader");
+                            break;
+                        default:
+                            Console.WriteLine("Det er null grader");
+                            break;
+                    }
+                    gyldig = true;
+                }
+                else
+                {
+                    Console.WriteLine("Tallet er ikke en temperatur. Prøv på nytt.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
